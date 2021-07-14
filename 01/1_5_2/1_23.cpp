3cdf379a1ea8e62e7d6488e33dc92f03b7bda002
Blaine-McK <6349076+Blaine-McK@users.noreply.github.com>
2021-07-14 08:14:20 +0100

    Completed exercises 1.23 and 1.24

--- 01/1_5_2/1_23.cpp
+++ 01/1_5_2/1_23.cpp
@@ -1,38 +1,51 @@
-  // C++ Primer Exercise 1.21
+// C++ Primer Exercise 1.23 and 1.24
 // By Blaine McKeever
-// 05/07/2021
+// 14/07/2021
 
 // Write a program that takes several transactions and counts how many transactions occur for each
 // ISBN
+
+// Test the program by reading in multiple transactions represeneting multiple ISBNs. Grouping
+// each ISBN togethe
 
 #include <iostream>
 #include "Sales_item.h"
 
 int main()
 {
+    // Counter to store number of transactions
+    int count = 0;
 
-    Sales_item currentItem, previousItem;
-    if (std::cin)
+    // Store read in item and previous item
+    Sales_item currentItem, nextItem;
+
+    // Read in first value and check that there is data to process
+    if (std::cin >> currentItem)
     {
-        int count = 1;
-        while (std::cin >> currentItem)
+        ++count;
+
+        // Read the rest of the transactions
+        while (std::cin >> nextItem)
         {
-            if (previousItem.isbn() == currentItem.isbn())
+            // If the next transaction has the same isbn as the current
+            if (currentItem.isbn() == nextItem.isbn())
             {
+                // Increment the counter
                 ++count;
             }
             else
             {
+                //Otherwise print the counter and reset to 1
                 std::cout << count << std::endl;
                 count = 1;
             }
-            previousItem = currentItem;
+
+            // Set the next item to current item
+            currentItem = nextItem;
         }
-    }
-    else
-    {
-        int count = 0;
-        std::cout << count << std::endl; 
+
+        // Print count for last value in file
+        std::cout << count << std::endl;
     }
 
     return 0;
