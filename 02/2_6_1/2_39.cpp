fd836896237e9c75f45c88a9f6148b0a682bbd16
Blaine-McK <6349076+Blaine-McK@users.noreply.github.com>
2021-09-03 12:13:06 +0100

    Completed exercise section 2.6.1

---
+++ 02/2_6_1/2_39.cpp
@@ -0,0 +1,11 @@
+// C++ Primer
+// Section 2.6.1
+// Exercise 2.39
+// By Blaine McKeever
+// 03/09/21
+
+struct Foo { /* empty  */ } // No semicolon
+int main()
+{
+    return 0;
+}
---
+++ 02/2_6_1/2_40.cpp
@@ -0,0 +1,16 @@
+// C++ Primer
+// Section 2.6.1
+// Exercise 2.40
+// By Blaine McKeever
+// 03/09/21
+
+#include <string>
+
+struct Sales_Data { std::string bookNo; unsigned units_sold = 0; double revenue = 0.0; };
+
+int main()
+{
+    
+    
+    return 0;
+}
